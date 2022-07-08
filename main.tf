e03fadd8cb871ad4825ee665e4b7d2a2da955d5c
tkumar123 <108876056+tkumar123@users.noreply.github.com>
2022-07-08 12:36:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "hw" {
-type = string
-default = "t2.nano"
+resource "aws_instance" "server2" {
+ ami = "ami-0a5b0c3ca9452f414"
+ instance_type = "t2.medium"
+ tags = {
+  Name = "Anil-tf-serverday5"
+  Env="Dev"
+Dep="Eng"
+ }
 }
 
